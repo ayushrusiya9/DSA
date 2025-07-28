41b17abc6219a42b8bd20047a2f9c0ec387e9cc7
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-28 22:59:38 +0530

    update

--- basic practice/check_prime_num.py
+++ basic practice/check_prime_num.py
@@ -1,25 +1,25 @@
-num = int(input("Enter Number: "))
+# num = int(input("Enter Number: "))
 
-#this is fast
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num == 2:
-        return True
-    if num % 2 == 0:
-        return False
-    for i in range(3,num,2):
-        if num % i == 0:
-            return False
-    
-    return True
+# #this is fast
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     if num == 2:
+#         return True
+#     if num % 2 == 0:
+#         return False
+#     for i in range(3,num,2):
+#         if num % i == 0:
+#             return False
 
-prime = is_prime(num)
+#     return True
 
-if prime == True:
-    print(f'Given number {num} is prime number.')
-else:
-    print(f'Given number {num} is not a prime number.')
+# prime = is_prime(num)
+
+# if prime == True:
+#     print(f'Given number {num} is prime number.')
+# else:
+#     print(f'Given number {num} is not a prime number.')
 
 
 #this is slow
@@ -32,3 +32,31 @@ else:
 #             return False
     
 #     return True
+
+def PrimeNum(n):
+    if n <= 1:
+        return False
+    
+    if n == 2:
+        return True
+    
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, num, 2):
+        if n % i == 0:
+            return False
+        return True
+
+            
+        
+        
+num = int(input("Enter Number: "))
+
+print(PrimeNum(num))
+
+
+# if :
+#     print(f"Given NUMBER {num} is a prime number.")
+# else:
+#     print(f"Given number {num} is not a prime number")
--- basic practice/sum_of_n_natural_number.py
+++ basic practice/sum_of_n_natural_number.py
@@ -1,19 +1,30 @@
+# n = int(input("Enter Number: "))
+
+# def sum_n_natural_num(n):
+    
+#     sum = 0
+#     for i in range(1, n + 1):
+#         sum = sum + i
+#         if i < n:
+#             print(i, end=' + ')
+#         else:
+#             print(i, end=' = ')
+    
+#     # print(sum)
+#     return sum
+
+# s = sum_n_natural_num(n)
+
+# print(s)
+
 n = int(input("Enter Number: "))
 
-def sum_n_natural_num(n):
-    
+def SumOfNaturalNum(n):
     sum = 0
-    for i in range(1, n + 1):
+    for i in range(1,n+1):
         sum = sum + i
-        if i < n:
-            print(i, end=' + ')
-        else:
-            print(i, end=' = ')
     
-    # print(sum)
     return sum
 
-s = sum_n_natural_num(n)
-
-print(s)
-
+nat = SumOfNaturalNum(n)
+print(nat)
