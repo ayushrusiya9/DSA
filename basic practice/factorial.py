e9f1128f60b0d1c1f78d7d47c3039806d186f4ab
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-30 23:10:50 +0530

    update

--- basic practice/factorial.py
+++ basic practice/factorial.py
@@ -1,12 +1,23 @@
+# n = int(input("Enter Number: "))
+
+# def factorial(num):
+#     fact = 1
+
+#     for i in range(n,1,-1):
+#         fact = fact * i
+    
+#     print(f'Factorial of a number {n} is {fact}')
+
+# factorial(n)
+
+
+
 n = int(input("Enter Number: "))
 
-def factorial(num):
-    fact = 1
+f = 1
+# i = n
+while(n > 0):
+    f = f * n
+    n = n - 1
 
-    for i in range(n,1,-1):
-        fact = fact * i
-    
-    print(f'Factorial of a number {n} is {fact}')
-
-factorial(n)
-
+print(f)
--- basic practice/perfect_number.py
+++ basic practice/perfect_number.py
@@ -1,18 +1,32 @@
-n = int(input("Enter Number: "))
+# n = int(input("Enter Number: "))
 
-def perfect_num(x):
-    sum = 0
-    for i in range(1, n+1):
-        if i < x:
-            if x % i == 0:
-                # print(i)
-                sum = sum + i
+# def perfect_num(x):
+#     sum = 0
+#     for i in range(1, n+1):
+#         if i < x:
+#             if x % i == 0:
+#                 # print(i)
+#                 sum = sum + i
     
-    return sum
+#     return sum
 
-result = perfect_num(n)
+# result = perfect_num(n)
 
-if n == result:
-    print(f'Given number {n} is perfect number.')
+# if n == result:
+#     print(f'Given number {n} is perfect number.')
+# else:
+#     print(f'Given number {n} is not perfect number')
+
+n = int(input("enter numner: "))
+
+sum = 0
+i = 1
+while(i < n):
+    if n % i == 0:
+        sum = sum + i
+    i = i + 1
+
+if sum == n:
+    print("perfect")
 else:
-    print(f'Given number {n} is not perfect number')  
+    print("not")
