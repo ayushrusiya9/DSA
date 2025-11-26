0de5c497f3a301d6c334b6e39bbac9d9d1979722
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-26 22:58:19 +0530

    update

---
+++ Coding-quetions/reverse_string.py
@@ -0,0 +1,12 @@
+def reverse_string_easy(s):
+    return s[::-1]
+
+word1 = "Hello World"
+word2 = "coding"
+
+print(f"Original: '{word1}'")
+print(f"Reversed: '{reverse_string_easy(word1)}'")
+
+print(f"Original: '{word2}'")
+print(f"Reversed: '{reverse_string_easy(word2)}'")
+
