93b7d1d40cf7b012684509d1b903c7754548601b
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-24 22:28:22 +0530

    update

--- Coding-quetions/palindrom_str.py
+++ Coding-quetions/palindrom_str.py
@@ -7,3 +7,15 @@ if s == rev_str:
     print("It's Palindrome")
 else:
     print("It's not palindrome")
+
+def palindrome(s):
+    rev_str = ""
+    for i in s:
+        rev_str = i + rev_str
+
+    if s == rev_str:
+        return True
+    else:
+        return False
+    
+print(palindrome("madam"))
